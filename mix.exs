63c07f9d91742eee237bb91aa40ecc6c42eab312
Nick Gunn <nick@ausimian.net>
2024-02-28 14:42:24 +1100

    0.2.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExMobileDevice.MixProject do
     [
       app: :exmobiledevice,
       description: "iDevice control via usbmuxd",
-      version: "0.2.6",
+      version: "0.2.7",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
